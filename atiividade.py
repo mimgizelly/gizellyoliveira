40278e4d1ebf89a11f00150329bf492c6c510917
mimgizelly <gizelly.oliveira@academico.ifpb.edu.br>
2025-07-25 13:43:38 +0000

    atiividade.py

---
+++ atiividade.py
@@ -0,0 +1,27 @@
+frutas=("banana","abacaxi","melancia","goibada","laraja")
+gasto_frutas = 1500
+gasto_arredamento = 800
+gasto_insumo = 600
+gasto_mundas =400
+gasto_mao_obra =1000
+
+gasto_total = gasto_frutas + gasto_arredamento + gasto_insumo + gasto_mundas +gasto_mao_obra
+
+print("as frutas escolhidasforam:banana,abacaxi,melancia,goibada,laraja ")
+print("gasto das frutas: 1500")
+print("gasto do arredimento:800")
+print("gasto de isumo: 600")
+print("gasto com as mudas das plantas:400")
+print("gasto de mao de obra:1000")
+
+#calculo final
+print()
+print("f + 1500(frutas)")
+print("f+ 800 (arreadamento)")
+print("f +  600 (insumo)")
+print("f + 400(mudas)")
+print("f + 1000(mao de obra)")
+print("f = 4300(gasto total)")
+
+
+      
--- projeto.py
+++ projeto.py
@@ -1,1 +1,5 @@
-print("olá mundo!")
+pares =[G for G in range(2, 22, 2)]
+print("os 20 primeiros números pares são:" ,pares)
+
+impares = [ Y for Y in range(1, 21, 2)]
+print("os 20 primeiros números pares são:" , impares)
